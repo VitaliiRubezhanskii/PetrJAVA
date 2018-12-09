0ace3053166e71a2374cec40aa6c5f266c536f5e
Vitalii Rubezhanskii <Vitalii.Rubezhanskii@gmail.com>
2018-12-09 10:00:12 +0200

    Security enhancement

--- src/main/java/com/petr/controller/UserController.java
+++ src/main/java/com/petr/controller/UserController.java
@@ -8,23 +8,28 @@ import com.petr.transport.dto.user.UserCreateDto;
 import com.petr.transport.dto.user.UserFindDto;
 import com.petr.transport.dto.user.UserOutcomeDto;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.multipart.support.DefaultMultipartHttpServletRequest;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -81,18 +86,17 @@ public class UserController {
         userService.uploadFiles(id, request);
     }
 
-    @GetMapping(value = "/user/{id}/document")
-    public ResponseEntity<byte []> downloadFile(@PathVariable("id") Long id){
-        User user=userService.getById(id);
-        File file = new File("/home/vitalii/Downloads/photoofmine.jpg");
-
+    @GetMapping(value = "/user/{id}/document" , produces = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_GIF_VALUE})
+    public void downloadFile(@PathVariable("id") Long id,  HttpServletResponse response) {
+        User user = userService.getById(id);
         try {
-            FileInputStream inputStream=new FileInputStream(file);
-
-            return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getName() + "\"")
-                    .body(Files.readAllBytes(Paths.get(file.getAbsolutePath())));
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(Files.readAllBytes(Paths.get(user.getPhoto())));
+            response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+            response.setHeader("Content-Transfer-Encoding", "binary");
+            response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" +user.getUsername()+ ".jpg");
+            StreamUtils.copy(inputStream, response.getOutputStream());
         }catch (IOException ex){ex.printStackTrace();}
-        return  ResponseEntity.status(404).body(null);
+
     }
 
     @GetMapping(value = "/{username}")
